0db881a8fe038dfa8e3e3a6bd9170cfd6f4a93c0
Emilien Aboula <300095450@fvtc.edu>
2025-12-18 23:28:12 -0600

    Update the Stage

--- SpaceFighter/BioEnemyShip.cpp
+++ SpaceFighter/BioEnemyShip.cpp
@@ -1,5 +1,7 @@
+
 #include "BioEnemyShip.h"
 #include "Level.h"
+
 
 BioEnemyShip::BioEnemyShip()
 {
@@ -8,24 +10,27 @@ BioEnemyShip::BioEnemyShip()
     SetCollisionRadius(20);
 }
 
+
 void BioEnemyShip::Update(const GameTime& gameTime)
 {
-    EnemyShip::Update(gameTime);
+	if (IsActive())
+	{
+		float x = sin(gameTime.GetTotalTime() * Math::PI + GetIndex());
+		x *= GetSpeed() * gameTime.GetElapsedTime() * 1.4f;
+		TranslatePosition(x, GetSpeed() * gameTime.GetElapsedTime());
 
-    if (!IsActive()) return;
+		if (!IsOnScreen()) Deactivate();
+	}
 
-    float x = sin(gameTime.GetTotalTime() * Math::PI + GetIndex());
-    x *= GetSpeed() * gameTime.GetElapsedTime() * 1.4f;
-    TranslatePosition(x, GetSpeed() * gameTime.GetElapsedTime());
-
-    if (!IsOnScreen())
-        Deactivate();
+	EnemyShip::Update(gameTime);
 }
+
 
 void BioEnemyShip::Draw(SpriteBatch& spriteBatch)
 {
-    if (!IsActive()) return;
-
-    const float alpha = GetCurrentLevel()->GetAlpha();
-    spriteBatch.Draw(m_pTexture, GetPosition(), Color::WHITE * alpha, m_pTexture->GetCenter(), Vector2::ONE, Math::PI, 1);
+	if (IsActive())
+	{
+		const float alpha = GetCurrentLevel()->GetAlpha();
+		spriteBatch.Draw(m_pTexture, GetPosition(), Color::WHITE * alpha, m_pTexture->GetCenter(), Vector2::ONE, Math::PI, 1);
+	}
 }
